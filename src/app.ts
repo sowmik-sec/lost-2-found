a2b8d8fb3e3031a215dff0150a115ea6e7425152
sowmik-sec <sowmik.sec@gmail.com>
2025-04-27 21:59:40 +0600

    separate middlewares from server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, { Application, Request, Response } from "express";
+import router from "./app/routes";
 export const app: Application = express();
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.json("Lost 2 Found");
 });
+
+app.use("/api/v1", router);
