3fa71411084e0f526d202dfe12107cde18c204c8
sowmik-sec <sowmik.sec@gmail.com>
2025-04-28 20:12:18 +0600

    create pagination helper

---
+++ src/helpers/paginationHelper.ts
@@ -0,0 +1,32 @@
+type TOptions = {
+  page?: number;
+  limit?: number;
+  sortOrder?: string;
+  sortBy?: string;
+};
+
+type TOptionsResult = {
+  page: number;
+  limit: number;
+  skip: number;
+  sortOrder: string;
+  sortBy: string;
+};
+const calculatePagination = (options: TOptions): TOptionsResult => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 5;
+  const skip: number = (page - 1) * limit;
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
+};
+
+export const paginationHelper = {
+  calculatePagination,
+};
