91fddd2f923e77616e9eed45bdebae65508028df
sowmik-sec <sowmik.sec@gmail.com>
2025-04-28 15:44:36 +0600

    add auth middleware in route

--- src/app/modules/FoundItemCategory/category.routes.ts
+++ src/app/modules/FoundItemCategory/category.routes.ts
@@ -2,10 +2,12 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { CategoryValidations } from "./category.validations";
 import { CategoryControllers } from "./category.controller";
+import auth from "../../middlewares/auth";
 const router = express.Router();
 
 router.post(
   "/found-item-categories",
+  auth(),
   validateRequest(CategoryValidations.createCategoryZodSchema),
   CategoryControllers.createCategory
 );
